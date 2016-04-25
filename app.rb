3210ebc05de647089eef12e82fbb42de443802b0
naridas <gregsparta@gmail.com>
2016-04-25 17:58:52 +0100

    wk3part12 complete

--- app.rb
+++ app.rb
@@ -9,7 +9,13 @@ get '/secret' do
 	'This is a secret page'
 end
 
-get '/cat' do
-	@random_names = ["Amy", "Grig", "Maria"].sample
+get '/random-cat' do
+	@names = ["Amy", "Grig", "Maria"].sample
+	erb(:index)
+end
+
+get '/named-cat' do
+	p params
+	@names = params[:name]
 	erb(:index)
 end
